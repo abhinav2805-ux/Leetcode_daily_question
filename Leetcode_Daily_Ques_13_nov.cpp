31386e417e80fdcccec310afea164f7ec25216d8
Abhinav Gupta <128059655+abhinav2805-ux@users.noreply.github.com>
2023-11-13 11:29:12 +0530

    Create Leetcode_Daily_Ques_13_nov.cpp

---
+++ Leetcode_Daily_Ques_13_nov.cpp
@@ -0,0 +1,27 @@
+
+
+                                                                      //2785
+
+class Solution {
+public:
+    string sortVowels(string s) {
+        string t = "";
+        for(int i =0;i<s.size();i++){
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U' ) {
+                t.push_back(s[i]);
+            }
+        }
+
+    sort(t.begin(),t.end());
+    int k =0;
+   for(int i =0;i<s.size();i++){
+            if((s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U' )) {
+                s[i] = t[k];
+                k++;
+            }
+        }
+
+return s;
+
+    }
+};
